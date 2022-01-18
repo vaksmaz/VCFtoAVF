1edb0d19bad597f1c80145bd29aab386e0b5ab9a
vaksmaz <47527925+vaksmaz@users.noreply.github.com>
2022-01-18 10:09:30 -0500

    Add files via upload

--- test/Test_run.sh
+++ test/Test_run.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-../VCF_to_AVF_run.pl -vcf GMLOF.new.vcf \
--ids BS_ER6ZP72Y,BS_PVCGJJ3D1,BS_C2GFCVS2 \
--rel proband,mother,tumor \
--tumor BS_C2GFCVS2 -o test.trio-zvtest \
--b hg38
+
+../VCF_to_AVF_run_01.pl -vcf GMLOF.new.multiFam.snpeff.vcf \
+	-b hg38 \
+	-snpSep -snpSepFL family_file.txt \
+	-short_fl \
+	-o Test-families
+
+##############################################
+
+#### Testing a set without inputing any individuals
+../VCF_to_AVF_run_01.pl -vcf GMLOF.new.multiFam.snpeff.vcf \
+	-b hg38 \
+	-snpSep -short_fl \
+	-o Test-indv
+
+
+
+##############################################
+
+###  testing directly inputing indivudal IDs
+../VCF_to_AVF_run_01.pl -vcf GMLOF.new.multiFam.snpeff.vcf \
+	-b hg38 \
+	-snpSep -short_fl \
+	-ids Pro01,Father01,Mother01 \
+	-rel Proband1,Father1,Mother1 \
+	-o Single_fam_test_commLine_input
+
+
+
+
+
+
+ 
